292dff0d4e0c153a304b6792b8261a09450e9a92
Devon Aimes <aimes.devon@gmail.com>
2022-10-01 13:58:57 -0400

    fixed terminal error messages

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get('/:number_of_bugs', (req,res) => {
             // <a href='/${Math.floor(Math.random) * 100}'>Look at next project</a>
     } else if (Math.random() < .3) {
         let randomBugs = bugsCount + randomNum
-        console.log(randomBugs)
+        // console.log(randomBugs)
         res.send(
             `
             <h1>${bugsCount} lines of code to debug</h1>
@@ -47,10 +47,19 @@ app.get('/:number_of_bugs', (req,res) => {
             `
             <h1>${bugsCount} lines of code to debug</h1>
             <h1>${bugsCount} lines of code</h1>
-            <a href='${randomBugs}'>look at one now, patch it around</a>
-            <h1>OH SWEET! ${randomBugs} lines of code to debug</h1>
+            <a href='${bugsCount - 1}'>look at one now, patch it around</a>
+            <h1>${bugsCount - 1} lines of code to debug</h1>
             `)
     } else if (Math.random() < .1 && bugsCount >150) {
+        
+        res.send(
+            `
+            <h1>${bugsCount / 2} lines of code to debug</h1>
+            <h1>${bugsCount / 2} lines of code</h1>
+            <a href='${(bugsCount / 2) - 1}'>look at one now, patch it around</a>
+            <h1>${(bugsCount / 2) - 1} lines of code to debug</h1>
+            `)
+    } else if (Math.random() < .1 && bugsCount >400) {
         
         res.send(
             `
@@ -58,15 +67,16 @@ app.get('/:number_of_bugs', (req,res) => {
             <h2>0 lines of code to debug!</h2>
             <a href='/'>Look at next project</a>
             `)
-    }
+    } else {
 
         res.send(
-        `
-        <h1>${bugsCount} lines of code to debug</h1>
+            `
+            <h1>${bugsCount} lines of code to debug</h1>
             <h1>${bugsCount} lines of code</h1>
             <a href='${bugsCount - 1}'>look at one now, patch it around</a>
             <h1>${bugsCount - 1} lines of code to debug</h1>
         `)
+    }
 
 })
 
